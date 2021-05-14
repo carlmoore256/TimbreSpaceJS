d90dcdbe8843f902f4948a9b19b79ec66c38fe56
carlmoore256 <carlmoore256@gmail.com>
2021-05-13 22:23:07 -0700

    meyda analysis cb generates grain objects

--- js/canvas.js
+++ js/canvas.js
@@ -1,22 +1,29 @@
 import * as THREE from './three/three.module.js';
 import Stats from './three/stats.module.js';
 import { OrbitControls } from './three/OrbitControls.js'
-
+// import GrainObject from './grain.js'
 
 let INTERSECTED, raycaster;
 const pointer = new THREE.Vector2;
 
-let renderer, stats, scene, camera, guiData;
+let audioContext, source;
+
+let m_MaxGrains = 10;
+
+let renderer, stats, m_Scene, camera, guiData, m_GrainParent;
+
+let m_AvgCenter = new THREE.Vector3();
 
 let m_Grains = [];
 
 init();
 animate();
 
+
 function init() {
 	// initialize scene
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xf0f0f0 );
+	m_Scene = new THREE.Scene();
+	m_Scene.background = new THREE.Color( 0x000000 );
 
 	// camera and renderer =========
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
@@ -31,8 +38,8 @@ function init() {
 	orbitControls.screenSpacePanning = true;
 
 	// framerate stats =========
-	stats = new Stats();
-	  document.body.appendChild( stats.dom );
+	// stats = new Stats();
+  // document.body.appendChild( stats.dom );
 
 	window.addEventListener( 'resize', onWindowResize );
 
@@ -43,18 +50,109 @@ function init() {
 	// gui options =========
 	guiData = {
 		currentSound: './audio/CISSA2.wav',
+		bufferSize: 1024,
+		hopSize: 512,
 	};
 
-	spawnGrains();
+	// audio context
+	// audioContext
+	const htmlAudioElement = document.getElementById("audio");
+	audioContext = new AudioContext();
+	source = audioContext.createMediaElementSource(htmlAudioElement);
+	source.connect(audioContext.destination);
+
+
+
+	// let color = new THREE.Color( 0, 0, 0 );
+	// const geometry = new THREE.SphereGeometry( 1, 8, 8 );
+	// const material = new THREE.MeshBasicMaterial( {color: color} );
+	// let object = new THREE.Mesh( geometry, material );
+	// m_Scene.add(object);
+
+	m_GrainParent = new THREE.Object3D();
+	m_Scene.add(m_GrainParent);
+
+	audioContext.resume();
+	// set up the feature extractor with callback
+	// for generating new grains (audio must be playing)
+	feature_extractor();
+}
+
+// get 3D axes from audio features
+function feature_extractor()
+{
+	let mfcc_coeffs = [];
+
+	const analyzer = Meyda.createMeydaAnalyzer({
+		"audioContext": audioContext,
+		"source": source,
+		"bufferSize": guiData.buffSize,
+		"hopSize": guiData.hopSize,
+		"featureExtractors": [ "mfcc", "loudness", "buffer", "rms" ], //buffer returns raw audio
+		"numberOfMFCCCoefficients": 6, //specify max mfcc coeffs
+		"callback": features => {
+
+			if (features.rms > 0.001)
+			{
+				let posScalar = 1;
+				let colScalar = 0.1;
+
+				let position = new THREE.Vector3( features.mfcc[0]*posScalar, features.mfcc[1]*posScalar, features.mfcc[2]*posScalar );
+				let color = new THREE.Color( features.mfcc[3]*colScalar, features.mfcc[4]*colScalar, features.mfcc[5]*colScalar );
+
+				let radius = features.rms * 10;
+
+				// color.setHex(0x44aa88);
+
+				// console.log(color);
+
+				// remember to add scale eventually
+				const geometry = new THREE.SphereGeometry( radius, 8, 8 );
+				const material = new THREE.MeshBasicMaterial( {color: color} );
+				let object = new THREE.Mesh( geometry, material );
+				object.parent = m_GrainParent;
+				// object.position.set(position.x, position.y, position.z);
+
+
+				let grain = new GrainObject(
+					object,
+					features.buffer,
+					position,
+					color,
+					features
+				)
+
+				m_Scene.add(object);
+				m_Grains.push(grain);
+			}
+
+		}
+	});
+	analyzer.start();
 }
 
 // input link to file, spawn grains in m_Grains
-function spawnGrains(audio_file) {
-	const geometry = new THREE.SphereGeometry( 5, 32, 32 );
-	const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-	const sphere = new THREE.Mesh( geometry, material );
-	scene.add( sphere );
-}
+// function spawnGrain(features) {
+// 	let position = new THREE.Vector3( features.mfcc[0], features.mfcc[1], features.mfcc[2] );
+// 	let color = new THREE.Color( features.mfcc[3], features.mfcc[4], features.mfcc[5] );
+// 	color.setHex(0x44aa88);
+//
+// 	// remember to add scale eventually
+// 	const geometry = new THREE.SphereGeometry( 1, 8, 8 );
+// 	// const material = new THREE.MeshBasicMaterial( {color: color} );
+// 	const material = new THREE.MeshBasicMaterial( {color: color} );
+// 	const object = new THREE.Mesh( geometry, material );
+//
+// 	const grain = new GrainObject(
+// 		object=object,
+// 		buffer=features.buffer,
+// 		position=position,
+// 		color=color,
+// 		features=features
+// 	)
+// 	m_Scene.add(object);
+// 	m_Grains.push(grain);
+// }
 
 function onPointerMove( event ) {
   pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -72,14 +170,20 @@ function animate() {
 	requestAnimationFrame( animate );
 
 	render();
-
-	stats.update();
+	// stats.update();
+	// let index = m_Grains.length-1;
+	// while (m_Grains.length > m_MaxGrains)
+	// {
+	//
+	// 	m_Scene.remove(m_Grains[m_Grains.length-1].object);
+	// 	m_Grains.splice(0, 1)
+	// }
 
 }
 
 function render() {
 	// raycastObjects();
-	renderer.render( scene, camera );
+	renderer.render( m_Scene, camera );
 }
 
 function raycastObjects()
@@ -100,3 +204,15 @@ function raycastObjects()
 
   }
 }
+
+// Existing code unchanged.
+window.onload = function() {
+  let audioContext = new AudioContext();
+}
+
+// One-liner to resume playback when user interacted with the page.
+document.querySelector('audio').addEventListener('play', function() {
+  audioContext.resume().then(() => {
+    console.log('Playback resumed successfully');
+  });
+});
